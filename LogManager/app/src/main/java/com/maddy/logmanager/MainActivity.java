bc79f8dcb45e2215c70bf89642f1aef59767c57a
Madhu <sooryaprasath19@gmail.com>
2015-10-05 20:23:13 -0400

    Log Manager

---
+++ LogManager/app/src/main/java/com/maddy/logmanager/MainActivity.java
@@ -0,0 +1,106 @@
+package com.maddy.logmanager;
+
+import java.sql.Date;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.CallLog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        getCallDetails();
+        Button btn = (Button) findViewById(R.id.dialler);
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent("android.intent.action.DIAL");
+                startActivity(intent);
+            }
+        };
+        btn.setOnClickListener(listener);
+    }
+
+    private void getCallDetails() {
+        StringBuffer sb = new StringBuffer();
+        String strOrder = android.provider.CallLog.Calls.DATE + " DESC";
+        Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, null,
+                null, null, strOrder);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+        while (managedCursor.moveToNext()) {
+            String phNum = managedCursor.getString(number);
+            String callTypeCode = managedCursor.getString(type);
+            String strcallDate = managedCursor.getString(date);
+            Date callDate = new Date(Long.valueOf(strcallDate));
+            String callDuration = managedCursor.getString(duration);
+            String callType = null;
+            int callcode = Integer.parseInt(callTypeCode);
+            switch (callcode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    callType = "Outgoing";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    callType = "Incoming";
+                    break;
+                case CallLog.Calls.MISSED_TYPE:
+                    callType = "Missed";
+                    break;
+            }
+            sb.append("\nPhone Number :" + phNum + " \nCall Type :"
+                    + callType + " \nCall Date :" + callDate
+                    + " \nCall duration :" + callDuration + "sec");
+            sb.append("\n..................................................................................................");
+            tv.setText(sb);
+        }
+        managedCursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void change(View v1) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setType("vnd.android-dir/mms-sms");
+        startActivity(intent);
+      }
+}
